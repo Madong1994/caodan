33b8019d38d05752cab42c5f69e1fd3ad6e6f8d3
Madong1994 <madong1170174224@163.com>
2019-04-18 00:01:19 +0800

    Initial commit

---
+++ src/main/java/com/md/caodan/CaodanApplication.java
@@ -0,0 +1,13 @@
+package com.md.caodan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaodanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaodanApplication.class, args);
+    }
+
+}
